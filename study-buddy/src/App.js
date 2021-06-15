12efd08fb798e819c12827176e5d648dbf547c8f
circuitmakr <circuitmakr@gmail.com>
2021-06-15 16:45:38 -0700

    testing 2

--- study-buddy/src/App.js
+++ study-buddy/src/App.js
@@ -14,7 +14,6 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-          rel= "new"
         >
           Learn React
         </a>
