bad2ca0313eb7ba9a5ab869d8d5cb765ce225ddb
Skyler Boyack <skylerboyack@gmail.com>
2021-06-15 18:38:13 -0600

    added server

---
+++ study-buddy/server/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+const cors = require('cors')
+
+const {CONNECTION_STRING, SESSION_SECRET, SERVER_PORT} = process.env
+
+const app = express()
+
+app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 1000 *60 *60 *24}
+}))
+
+// massive({
+//     connectionString: CONNECTION_STRING,
+//     ssl: {rejectUnauthorized: false}
+// })
+// .then(db => {
+//     app.set('db', db)
+//     console.log('db connected')
+// })
+// .catch(err => console.log(err)) 
+app.listen(SERVER_PORT, () => console.log(`Server is listening on ${SERVER_PORT}`))
